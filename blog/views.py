febf128a71bc770a22306ac591c92a01ff500c1d
Anonvia <sheel1997sanjay@gmail.com>
2020-07-12 15:38:08 +0530

    done

--- blog/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return HTTPResponse("hello ths is index page")
-    
-def another(request):
-    return HTTPResponse("hello ths is another page")
--- home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-urlpatterns = [
-    path('',views.home,name='home'),
-    path('contact/',views.contact,name='contact'),  
-    path('about/',views.about,name='about'),  
-
-]
