048ec27a29664bde2e2517d7d442a5a503257220
Anonvia <sheel1997sanjay@gmail.com>
2020-07-12 15:28:03 +0530

    done

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('',views.index,name='index'),  
+    path('another/',views.index,name='another'),  
 ]
--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('',views.home,name='home'),
-    path('contact',views.contact,name='contact'),  
+    path('contact/',views.contact,name='contact'),  
     path('about/',views.about,name='about'),  
+
 ]
