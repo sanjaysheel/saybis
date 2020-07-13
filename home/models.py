e7f6d0069abbc24afd3b42f754156693d0f6ed59
Anonvia <sheel1997sanjay@gmail.com>
2020-07-13 17:20:27 +0530

    model and view

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    seriel_number = models.IntegerField(primary_key=10)
+    first_name = models.CharField(max_length=10)
+    last_name = models.CharField(max_length=10)
+    email_id = models.CharField(max_length=20)
+    state = models.CharField(max_length=20,default='')
+    city = models.CharField(max_length=20,default='')
+    desc = models.CharField(max_length=500,default='')
+    contact_number = models.IntegerField(max_length=14,default='')
+    time = models.DateTimeField(auto_now_add=True,blank=True)
+
+    def __str__(self):
+        return 'message from ' +  self.first_name + ' ' + self.last_name
--- home/views.py
+++ home/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
+from  .models import Contact
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,'home/home.html')
 
 def contact(request):
+    if request.method == 'post':
+        fname = request.POST['firstname']
+        lname = request.POST['lastname']
+        email = request.POST['inputEmail4']
+        contact = request.POST['contact_number']
+        state = request.POST['statename']
+        city = request.POST['city']
+        desc = request.POST['desc']
+        con = Contact(first_name= fname,last_name=lname,email_id=email,state=state,city=city,desc=desc,contact_number=contact)
+        con.save()
     return render(request,'home/contact.html')
 
 def about(request):
