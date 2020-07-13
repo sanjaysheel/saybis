fcf8c89987cfb0e21c51b449ec39d46e22ce7edb
Anonvia <sheel1997sanjay@gmail.com>
2020-07-13 15:14:47 +0530

    This is my 5th commit

--- home/views.py
+++ home/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return render(request,'./home/home.html')
+    return render(request,'home/home.html')
 
 def contact(request):
-    return HttpResponse('contact')
+    return render(request,'home/contact.html')
 
 def about(request):
-    return HttpResponse('about')
+    return render(request,'home/about.html')
